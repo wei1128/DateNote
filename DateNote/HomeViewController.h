f7048e472bd26e99c82aebce15acd38cbf5584bd
brianhph <brianhph@yahoo-inc.com>
2015-07-13 16:57:24 +0800

    add project

---
+++ DateNote/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  DateNote
+//
+//  Created by Brian Huang on 7/13/15.
+//  Copyright (c) 2015 EC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ DateNote/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  DateNote
+//
+//  Created by Brian Huang on 7/13/15.
+//  Copyright (c) 2015 EC. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "KDCalendarView.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
