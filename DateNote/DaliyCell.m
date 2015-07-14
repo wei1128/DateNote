8b6ac07664deb1c3d18d25848c64a55b8733e229
mchsieh <mchsieh@yahoo-inc.com>
2015-07-14 19:21:42 +0800

    daily view controller

---
+++ DateNote/DaliyCell.m
@@ -0,0 +1,23 @@
+//
+//  DaliyCell.m
+//  DateNote
+//
+//  Created by Man-Chun Hsieh on 7/13/15.
+//  Copyright (c) 2015 EC. All rights reserved.
+//
+
+#import "DaliyCell.h"
+
+@implementation DaliyCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
