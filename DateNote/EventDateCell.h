ac936fe556f470810d1e53f6c49e013eb4b4212e
jackal <jackal@yahoo-inc.com>
2015-07-29 17:31:21 +0800

    add item tableview

---
+++ DateNote/EventDateCell.h
@@ -0,0 +1,15 @@
+//
+//  EventDateCell.h
+//  DateNote
+//
+//  Created by Jackal Wang on 2015/7/29.
+//  Copyright (c) 2015年 EC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDateCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *eventDate;
+@property (weak, nonatomic) IBOutlet UILabel *eventTemplateName;
+
+@end
---
+++ DateNote/EventDescCell.m
@@ -0,0 +1,23 @@
+//
+//  EventDescCell.m
+//  DateNote
+//
+//  Created by Jackal Wang on 2015/7/29.
+//  Copyright (c) 2015年 EC. All rights reserved.
+//
+
+#import "EventDescCell.h"
+
+@implementation EventDescCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
